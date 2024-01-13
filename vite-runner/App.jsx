67cd2fe51a81cdf824933e3fe815a433b94edfa9
sundada <956593305@qq.com>
2024-01-13 20:37:00 +0800

    suport jsx

---
+++ vite-runner/App.jsx
@@ -0,0 +1,9 @@
+
+import React from "./Core/React.js"
+
+
+
+// const App = React.createElement('div', {id: 'app'}, 'hello->', 'world')
+const App = <div id="app">hello --- world</div>
+
+export default App
---
+++ vite-runner/main.js
@@ -0,0 +1,5 @@
+import App from "./App.jsx"
+import ReactDOM from "./Core/ReactDOM.js"
+
+ReactDOM.createRoot(document.getElementById('root')).render(App)
+
